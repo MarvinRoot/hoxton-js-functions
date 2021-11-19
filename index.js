719e22f4d3d4f4307f14762994d177577f0de2e1
marvin <aliajmarvin@gmail.com>
2021-11-19 18:29:57 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// Input: a (number), b (number)
+// Action: Add both numbers together
+// Output: The result (number)
+function add() {
+    // write your code here
+  }
+  
+  // Input: number
+  // Action: Check if the number given is even or not
+  // Output: The result (boolean)
+  function isEven() {
+    // write your code here
+  }
+  
+  // Input: name (string)
+  // Action: Greet the user by name
+  // Output: The greeting (string)
+  function greet() {
+    // write your code here
+  }
+  
+  // Input: age (number)
+  // Action: Check if the age is 18 or over
+  // Output: The result (boolean)
+  function isAnAdult() {
+    // write your code here
+  }
+  
+  // Input: age (number)
+  // Action: Check how many years are left until adulthood (18)
+  // Output: The result (number)
+  function yearsToAdulthood() {
+    // write your code here
+  }
+  
+  // Input: person ({ age: number, name: string })
+  // Action:
+  //   - check if a person is an adult
+  //   - if they are, greet them
+  //   - if they are not, tell them to come back in X years (when they are)
+  // Output: The result (string)
+  function admit() {
+    // write your code here
+    // use greet, isAnAdult and yearsToAdulthood to help you!
+  }
